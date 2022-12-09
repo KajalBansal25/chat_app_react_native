ab19a945cd23ff8f557a0c7b19343adf755b011d
KajalBansal25 <kajal.bansal@daffodilsw.com>
2022-12-09 09:10:07 +0530

    jjj

--- App.js
+++ App.js
@@ -77,7 +77,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back seetyuiuytyuiouytyuiouyuiouyuiouyuiuyuiuyiouyio your edits.
+            screen and then come back seeyuiouyuiuyuiuyiouyio your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
